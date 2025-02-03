5438f75dafd68b2233d381fa9318cec3732b1e74
Jose Daniel <josedaniel67>
2025-02-03 19:57:05 -0400

    second commit

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from "react";
+import { useState } from "react";
 import Display from "./Display";
 import Button from "./Button";
 
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,6 +1,3 @@
-'use client';
-import React from "react";
-
 const Display = ({ value }: { value: string }) => {
   return (
     <div className="bg-gray-700 p-4 text-right text-2xl font-mono rounded-lg text-white">
